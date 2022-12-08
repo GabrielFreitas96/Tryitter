2c3ade814b3acee2a1e14b6ee83f5f53b1eb5892
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-12-08 18:41:15 -0300

    feat: Criando as models

---
+++ Tryitter/Tryitter/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tryitter.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Content { get; set; } = null!;
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+    }
+}
+
---
+++ Tryitter/Tryitter/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Tryitter.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public IEnumerable<Post> Posts { get; set; } = new List<Post>(); 
+    }
+}
+
